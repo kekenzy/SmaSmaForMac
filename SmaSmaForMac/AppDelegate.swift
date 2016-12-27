e6f68486fa7a129d1b002fa24dbe8be39f3c5db1
ADK114019 <ADK114019@adk114019-no-imac.local>
2016-12-27 16:54:18 +0900

    Initial Commit

---
+++ SmaSmaForMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmaSmaForMac
+//
+//  Created by ADK114019 on 2016/12/27.
+//  Copyright © 2016年 ADK114019. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SmaSmaForMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmaSmaForMac
+//
+//  Created by ADK114019 on 2016/12/27.
+//  Copyright © 2016年 ADK114019. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
