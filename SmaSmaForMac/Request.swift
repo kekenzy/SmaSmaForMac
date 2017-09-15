4fad4c012d742fdb6e35a6c6363c6502c9fd9327
Kenji Nagai <nagai.kenji@devlocal.org>
2017-09-15 09:27:32 +0900

    2017/09/15 安定版更新

---
+++ SmaSmaForMac/Request.swift
@@ -0,0 +1,65 @@
+//
+//  Request.swift
+//  SmaSmaForMac
+//
+//  Created by ADK114019 on 2017/01/11.
+//  Copyright © 2017年 ADK114019. All rights reserved.
+//
+
+import Foundation
+
+class Request {
+    let session: URLSession = URLSession.shared
+    
+    // GET METHOD
+    func get(url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        var request: URLRequest = URLRequest(url: url)
+        
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        session.dataTask(with: request, completionHandler: completionHandler).resume()
+    }
+    
+    // POST METHOD
+    func post(url: URL, body: NSMutableDictionary, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
+        var request: URLRequest = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+        
+        session.dataTask(with: request, completionHandler: completionHandler).resume()
+    }
+    
+    // PUT METHOD
+    func put(url: URL, body: NSMutableDictionary, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
+        var request: URLRequest = URLRequest(url: url)
+        
+        request.httpMethod = "PUT"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+        session.dataTask(with: request, completionHandler: completionHandler).resume()
+    }
+    
+    // PATCH METHOD
+    func patch(url: URL, body: NSMutableDictionary, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) throws {
+        var request: URLRequest = URLRequest(url: url)
+        
+        request.httpMethod = "PATCH"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = try JSONSerialization.data(withJSONObject: body, options: JSONSerialization.WritingOptions.prettyPrinted)
+        session.dataTask(with: request, completionHandler: completionHandler).resume()
+    }
+    
+    // DELETE METHOD
+    func delete(url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        var request: URLRequest = URLRequest(url: url)
+        
+        request.httpMethod = "DELETE"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        session.dataTask(with: request, completionHandler: completionHandler).resume()
+    }
+}
--- SmaSmaForMac/ViewController.swift
+++ SmaSmaForMac/ViewController.swift
@@ -7,12 +7,29 @@
 //
 
 import Cocoa
+import BleFormatterForMac
+
+let SERVICE_UUID = "0BF806E9-F1FE-4326-AB21-CEAFDEAFE0E0"
+let CHR_UUID_1 = "F5A6373E-B756-4B55-BE51-F8BD9A4A3193"
+//let CHR_UUID_2 = "BC24FB53-E185-4C05-8221-FFF92242914F"
 
 class ViewController: NSViewController {
 
+    var bleManager:BleManager!;
+    var dataFormat:[BleDataFormat] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.bleManager = BleManager.sharedInstance;
+        self.bleManager.setUUID(serviceUUID: SERVICE_UUID)
+        self.dataFormat.append(BleDataFormat(uuid: CHR_UUID_1, dataType: .String))
+//        self.dataFormat.append(BleDataFormat(uuid: CHR_UUID_2, dataType: .String))
+        self.bleManager.setReadDataFormat(bleDataFormat: self.dataFormat)
+        
+        // observer登録
+        self.bleManager.addObserver(self, withRead: #selector(ViewController.readNotification(_:)))
+        self.bleManager.addObserver(self, withWrite: #selector(ViewController.writeNotification(_:)))
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +39,129 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func onClickButton(_ sender: Any) {
+        bleManager.write(withString: "test", dataFormat: &self.dataFormat[0])
 
+    
+    
+    }
+
+    @IBAction func onClickSendHTTP(_ sender: Any) {
+//        sendSmartDeviceData()
+
+    }
+    
+    func sendSmartDeviceData(_ message:String) {
+        let request: Request = Request()
+        
+        
+//        no=C93057&user=kenji&used=false
+        let urlTmp = "http://localhost:8000/insertEnginSpeed.php" + message
+        let url: URL = URL(string: urlTmp)!
+        let body: NSMutableDictionary = NSMutableDictionary()
+        body.setValue("value", forKey: "key")
+        
+        do {
+
+            try request.get(url: url, completionHandler: { data, response, error in
+                if let dataValue = data {
+                    print("\(dataValue)")
+                    do {
+                        let jsonNode = try JSONSerialization.jsonObject(with: dataValue, options: .allowFragments)
+                        print("\(jsonNode)")
+                        
+                    } catch {
+                        
+                    }
+                    
+                }
+            })
+//        try request.post(url: url, body: body, completionHandler: { data, response, error in
+//            print("\(data)")
+            // code
+//        })
+        } catch {
+            
+        }
+    }
+    
+    func sendSampleLaravel() {
+        let request: Request = Request()
+        
+        let url: URL = URL(string: "http://172.28.36.66:8000/smartdata/3")!
+//        let url: URL = URL(string: "http://localhost:8000/smartphones")!
+        let body: NSMutableDictionary = NSMutableDictionary()
+        body.setValue("value", forKey: "key")
+        
+        do {
+
+            try request.get(url: url, completionHandler: { data, response, error in
+                            print("\(data)")
+                do {
+                    let jsonNode = try JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                    print("\(jsonNode)")
+                
+                } catch {
+                    
+                }
+            })
+//        try request.post(url: url, body: body, completionHandler: { data, response, error in
+//            print("\(data)")
+            // code
+//        })
+        } catch {
+            
+        }
+        
+    }
+    
+    func readNotification(_ notification: Notification) {
+        // 変数宣言時にアンラップ & キャストする方法
+//        let userInfo = [BleDataKey.NOTIFY_READ.description():dataFormatList[index].characteristicUUID.description,
+//                        BleDataKey.VALUE.description():dataFormatList[index].value] as [String : Any]
+        //                        let value = self.dataFormatList[index].getStringFromValue()
+        var tmpValue:String?
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+        let data = userInfo[BleDataKey.NOTIFY_READ.description()] as! BleDataFormat
+        switch data.dataType {
+        case .String:
+            tmpValue = data.getStringFromValue() as String
+        case .Int:
+            let i = data.getIntFromValue() as Int
+            print("read Int value = \(i)")
+            tmpValue = data.getStringFromValue() as String
+        case .Double:
+            let i = data.getDoubleFromValue() as Double
+            print("read Double value = \(i)")
+            tmpValue = data.getStringFromValue() as String
+        case .Data:
+            let i = data.getDataFromValue() as Data
+            print("read Data value = \(i)")
+            tmpValue = data.getStringFromValue() as String
+        }
+        
+        guard let value = tmpValue else {
+            print("can not read Notification message!!")
+            return
+        }
+        print("read Notification message  =  \(value)")
+        sendSmartDeviceData(value)
+        
+        let postName = data.getPostName() as String
+        
+//        self.myTalkFlg = false
+//        talkList.insert(value + "," + postName, at: 0)
+//        let indexPath = IndexPath(row: 0, section: 0)
+//        self.tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    func writeNotification(_ notification: Notification) {
+        // 変数宣言時にアンラップ & キャストする方法
+        
+//        self.msgText.text = ""
+        return
+    }
 }
 
